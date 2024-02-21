61539c7823c4f97d3092af91550e0a83d50ffaf5
QuiebreCultural <elquiebrecultural@gmail.com>
2024-02-21 17:30:16 -0300

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+/* eslint-disable no-unused-vars */
 import './App.css'
 import Navbar from './components/Navbar'
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import Who from './components/Who';
 
 function App() {
 
+  const navigate = useNavigate(); // Hook useNavigate para redirección
+
   return (
     <>
-    <Navbar/>
+      <Navbar />
+      <button className='whoButton' onClick={() => navigate('/who')}><span className='whoSpan'>¿Quiénes somos?</span></button>
+      <Routes>
+        <Route path="/"/>
+        <Route path="/who" element={<Who />} />
+        {/* Otras rutas aquí */}
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/Who.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+function Who() {
+  return (
+    <div>Who</div>
+  )
+}
+
+export default Who
